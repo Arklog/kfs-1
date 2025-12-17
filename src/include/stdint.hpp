408585a9da56a7b3b1a43a4beb4c54377ad028ff
Pierre <pierr.ducloux@gmail.com>
2025-12-17 22:22:13 +0100

    added custom stdint

---
+++ src/include/stdint.hpp
@@ -0,0 +1,20 @@
+//
+// Created by pierre on 12/17/25.
+//
+
+#ifndef KFS_1_STDINT_HPP
+#define KFS_1_STDINT_HPP
+
+using uint8_t = unsigned char;
+using int8_t = signed char;
+
+using uint16_t = unsigned short;
+using int16_t = signed short;
+
+using uint32_t = unsigned int;
+using int32_t = signed int;
+
+using uint64_t = unsigned long long;
+using int64_t = signed long long;
+
+#endif //KFS_1_STDINT_HPP
