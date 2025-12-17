c86165bb23ac76bb90e25ed148eb2215a5b7602b
Pierre <pierr.ducloux@gmail.com>
2025-12-17 22:22:32 +0100

    libvga first commit

---
+++ src/arch/x86_64/vga/vga.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pierre on 12/17/25.
+//
+#include "vga.hpp"
+
+kernel::vga::t_vga_char kernel::vga::vga_get_color(uint8_t character, color::vga_color fg, color::vga_color bg) {
+    t_vga_char vga_char;
+
+    vga_char.data.ascii = character;
+    vga_char.data.color = (bg << 4) | (fg & 0x0F);
+
+    return vga_char;
+}
