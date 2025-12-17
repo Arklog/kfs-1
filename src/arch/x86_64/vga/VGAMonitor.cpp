622937524fba168d1077bd58c27c22b74b66fb98
Pierre <pierr.ducloux@gmail.com>
2025-12-17 23:54:45 +0100

    VGAMonitor first implementation

---
+++ src/arch/x86_64/vga/VGAMonitor.cpp
@@ -0,0 +1,23 @@
+//
+// Created by pierre on 12/17/25.
+//
+
+#include "VGAMonitor.hpp"
+#include "vga.hpp"
+
+# define VGA_ADDRESS 0xB8000
+
+namespace kernel::vga {
+    const uint16_t *VGAMonitor::_beg = reinterpret_cast<const uint16_t *>(VGA_ADDRESS);
+    uint16_t *VGAMonitor::_pos = reinterpret_cast<uint16_t *>(VGA_ADDRESS);
+
+    VGAMonitor::VGAMonitor() = default;
+
+    VGAMonitor & VGAMonitor::operator<<(const char *data) {
+        while (*data) {
+            auto character = vga_get_color(*data);
+            *_pos++ = character.raw;
+        }
+        return *this;
+    }
+}
---
+++ src/arch/x86_64/vga/VGAMonitor.hpp
@@ -0,0 +1,23 @@
+//
+// Created by pierre on 12/17/25.
+//
+
+#ifndef KFS_1_VGAMONITOR_HPP
+#define KFS_1_VGAMONITOR_HPP
+
+#include "include/stdint.hpp"
+
+namespace kernel::vga {
+    class VGAMonitor {
+    private:
+        static const uint16_t *_beg;
+        static uint16_t *_pos;
+
+    public:
+        VGAMonitor();
+
+        VGAMonitor& operator<<(const char *data);
+    };
+}
+
+#endif //KFS_1_VGAMONITOR_HPP
