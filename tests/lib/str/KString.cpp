97e9ad49eb3fcca830f4b917d47eb04b880161e3
Pierre <pierr.ducloux@gmail.com>
2025-12-19 15:56:13 +0100

    Added missing test and test cases

--- tests/lib/str/KString.cpp
+++ tests/lib/str/KString.cpp
@@ -10,37 +10,40 @@ TEST_CASE("KString", "[kstring]") {
         const char *str = "strken";
         const char *str2 = "1";
         const char *str3 = "";
+        const char *str4 = "a\0b";
 
         REQUIRE(kstring::strlen(str) == 6);
         REQUIRE(kstring::strlen(str2) == 1);
         REQUIRE(kstring::strlen(str3) == 0);
+        REQUIRE(kstring::strlen(str4) == 1);
     }
 
     SECTION("strcmp") {
         const char *str1 = "str1";
         const char *str2 = "str2";
+        const char *str4 = "str1\0a";
         const char *str3 = "";
-        const char *str4 = "str1";
 
         REQUIRE(kstring::strcmp(str1, str2) == -1);
         REQUIRE(kstring::strcmp(str1, str1) == 0);
         REQUIRE(kstring::strcmp(str1, str3) == 115);
+        REQUIRE(kstring::strcmp(str1, str4) == 0);
     }
 
-    //SECTION("strncmp") {
-     //   const char *str1 = "str1";
-    //    const char *str2 = "str2";
-    //    const char *str3 = "";
-    //    const char *str4 = nullptr;
+    SECTION("strncmp") {
+        const char *str1 = "str1";
+        const char *str2 = "str2";
+        const char *str4 = "at";
+        const char *str3 = "";
 
-        //REQUIRE(kstring::strncmp(str1, str2, 3) == -1);
-        //REQUIRE(kstring::strncmp(str1, str2, 2) == 0);
-        //REQUIRE(kstring::strncmp(str1, str2, 19) == -1);
-        //REQUIRE(kstring::strncmp(str1, str1, 10) == 0);
-        //REQUIRE(kstring::strncmp(str1, str3, 1) == 115);
-        //REQUIRE(kstring::strncmp(str1, str3, 0) == 115);
-    //    REQUIRE(kstring::strncmp(str3, str4, 10) == 0);
-//    }
+        REQUIRE(kstring::strncmp(str1, str2, 0) == 0);
+        REQUIRE(kstring::strncmp(str1, str2, 3) == 0);
+        REQUIRE(kstring::strncmp(str1, str2, 4) == -1);
+        REQUIRE(kstring::strncmp(str3, str1, 1) == -115);
+        REQUIRE(kstring::strncmp(str1, str3, 1) == 115);
+        REQUIRE(kstring::strncmp(str1, str4, 1) == 's' - 'a');
+        REQUIRE(kstring::strncmp(str1, str2, 10) == '1' - '2');
+    }
 
     SECTION("strchr") {
         const char *str1 = "Hello kernel";
@@ -75,6 +78,34 @@ TEST_CASE("KString", "[kstring]") {
         REQUIRE(kstring::strstr(str1, str4) == nullptr);
         REQUIRE(kstring::strstr(str1, str5) == nullptr);
         REQUIRE(kstring::strstr(str1, str6) == nullptr);
+    }
+
+    SECTION("strcpy") {
+        char buffer[128];
+        const char *str1 = "Hello kernel";
+
+        kstring::strcpy(buffer, "");
+        REQUIRE(std::strcmp(buffer, "") == 0);
+
+        kstring::strcpy(buffer, str1);
+        REQUIRE(std::strcmp(buffer, str1) == 0);
+
+        kstring::strcpy(buffer, "a\0b");
+        REQUIRE(std::strcmp(buffer, "a") == 0);
+        REQUIRE(std::strlen(buffer) == 1);
+    }
+
+    SECTION("memcpy") {
+        char dest[128];
+        char src[128];
+
+        dest[0] = 1;
+        kstring::memcpy(dest, src, 0);
+        REQUIRE(dest[0] == 1);
+
+        std::memset(src, 1, 128);
+        kstring::memcpy(dest, src, 128);
+        REQUIRE(std::memcmp(dest, src, 128) == 0);
     }
 
     SECTION("safe_atoi") {
