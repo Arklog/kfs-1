1e8eaf99e0c70be92e6c6f0a0ce8ccc8d8a08426
Pierre <pierr.ducloux@gmail.com>
2025-12-20 16:32:29 +0100

    added stream color modifier

--- src/arch/i386/vga/VGAStreamModifier.cpp
+++ src/arch/i386/vga/VGAStreamModifier.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "VGAStreamModifier.hpp"
-
 #include "VGACursor.hpp"
 
 void operator delete(void *) {
@@ -13,8 +12,52 @@ void operator delete(void *, unsigned int) {}
 void operator delete(void *, long unsigned int) {
 }
 
-namespace vga {
-    VGASetLineModifier::VGASetLineModifier(uint16_t line): VGAStreamModifier(), _line{line} {
+namespace vga::modifier {
+    //
+    // VGAStreamColorModifier
+    //
+    VGAStreamColorModifier::VGAStreamColorModifier(uint8_t color): VGAStreamModifier(), _color{color} {
+    }
+
+    VGAStreamColorModifier::VGAStreamColorModifier(color::vga_color foreground, color::vga_color background):
+        VGAStreamModifier(),
+        _color{static_cast<uint8_t>(background << 4 | (foreground & 0x0F))} {
+    }
+
+    void VGAStreamColorModifier::operator()(uint8_t & color) const {
+        color = _color;
+    }
+
+    //
+    // VGAStreamBackgroundModifier
+    //
+    VGAStreamBackgroundModifier::VGAStreamBackgroundModifier(uint8_t bg_color): VGAStreamColorModifier(bg_color) {
+    }
+
+    VGAStreamBackgroundModifier::VGAStreamBackgroundModifier(color::t_vga_color bg_color): VGAStreamColorModifier(bg_color) {
+    }
+
+    void VGAStreamBackgroundModifier::operator()(uint8_t &color_byte) const {
+        color_byte = static_cast<uint8_t>(_color << 4 | (color_byte & 0x0F));
+    }
+
+    //
+    // VGAStreamForegroundModifier
+    //
+    VGAStreamForegroundModifier::VGAStreamForegroundModifier(uint8_t foreground): VGAStreamColorModifier(foreground) {
+    }
+
+    VGAStreamForegroundModifier::VGAStreamForegroundModifier(color::vga_color foreground): VGAStreamColorModifier(foreground) {
+    }
+
+    void VGAStreamForegroundModifier::operator()(uint8_t &color_byte) const {
+        color_byte = static_cast<uint8_t>((color_byte & 0xF0) | (color_byte & 0x0F));
+    }
+
+    //
+    // VGASetLineModifier
+    //
+    VGASetLineModifier::VGASetLineModifier(uint16_t line): VGAStreamCursorModifier(), _line{line} {
     }
 
     void VGASetLineModifier::operator()(VGACursor &cursor) const {
--- src/arch/i386/vga/vga.cpp
+++ src/arch/i386/vga/vga.cpp
@@ -11,23 +11,22 @@ uint8_t vga::t_vga_char::get_background() const {
     return (data.color >> 4) & 0x0F;
 }
 
-void vga::t_vga_char::set_foreground(color::t_vga_color color) {
+void vga::t_vga_char::set_foreground(const color::t_vga_color color) {
     data.color = (data.color & 0xF0) | (color & 0x0F);
 }
 
-void vga::t_vga_char::set_background(color::t_vga_color color) {
+void vga::t_vga_char::set_background(const color::t_vga_color color) {
     data.color = (color << 4) | (data.color & 0x0F);
 }
 
-void vga::t_vga_char::set_color(color::t_vga_color foreground, color::t_vga_color background) {
+void vga::t_vga_char::set_color(const color::t_vga_color foreground, const color::t_vga_color background) {
     data.color = (background << 4) | (foreground & 0x0F);
 }
 
-vga::t_vga_char vga::vga_get_color(uint8_t character, color::vga_color fg, color::vga_color bg) {
-    t_vga_char vga_char;
-
-    vga_char.data.ascii = character;
-    vga_char.data.color = (bg << 4) | (fg & 0x0F);
+vga::t_vga_char vga::vga_get_color(const uint8_t character, const color::vga_color fg, const color::vga_color bg) {
+    const t_vga_char vga_char{
+        .raw = static_cast<uint16_t>(character | bg << 12 | (fg & 0x0F) << 8),
+    };
 
     return vga_char;
 }
