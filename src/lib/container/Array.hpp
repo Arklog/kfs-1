14f97effc8fa557981b0923333ccc41b2918e5e7
Pierre <pierr.ducloux@gmail.com>
2026-01-07 14:08:02 +0100

    updated array

--- src/lib/container/Array.hpp
+++ src/lib/container/Array.hpp
@@ -7,7 +7,6 @@
 
 #include "Container.hpp"
 #include "Iterator.hpp"
-#include "lib/str/KString.hpp"
 #include "lib/utility/utility.hpp"
 
 namespace container {
@@ -21,24 +20,62 @@ namespace container {
 
         Array() = default;
 
-        Array(T data[N]) {
-            for (unsigned i = 0; i < N; ++i) {
-                this->_data[i] = data[i];
-            }
-        }
-
+        /**
+         * Create array with elements
+         *
+         * @tparam Args
+         * @param args
+         */
         template<typename... Args>
         Array(Args... args) :
             _data{args...} {
+            static_assert(sizeof...(Args) <= N, "Too many elements");
         }
 
+        /**
+         * Create array with elements from _begin to _end at the beginning
+         *
+         * @param _begin
+         * @param _end
+         */
         Array(iterator _begin, iterator _end) {
             auto i = 0;
-            while (_begin < _end) {
+            while (_begin < _end && i < N) {
                 _data[i++] = *(_begin++);
             }
         }
 
+        /**
+         * Copy constructor
+         * @param other
+         */
+        Array(const Array &other) {
+            auto idx = 0;
+            for (const auto &i: other) {
+                _data[idx++] = i;
+            }
+        }
+
+        /**
+         * Move constructor
+         * @param other
+         */
+        Array(Array &&other) noexcept {
+            auto idx = 0;
+
+            for (auto &i: other) {
+                _data[idx++] = utility::move(i);
+            }
+        }
+
+        /**
+         * Allow concatenation of two Array of size N and N1
+         *
+         * @tparam N1 The size of the second array
+         * @param other The array to concatenate with current array
+         *
+         * @return An array of size N + N1, being the concatenation of this array and other
+         */
         template<unsigned N1>
         Array<T, N + N1> operator+(const Array<T, N1> &other) const {
             Array<T, N + N1> result;
@@ -92,12 +129,24 @@ namespace container {
         size_type size() const override { return N; };
 
         template<typename... Args>
-        void emplace(const iterator &iter, Args &&... args) {
+        iterator emplace(iterator iter, Args &&... args) {
+            if (_check_iterator(iter))
+                return end();
+
             new(static_cast<T *>(iter)) T(args...);
+            return iter;
+        }
+
+        iterator emplace(iterator position, T &&value) {
+            if (!_check_iterator(position))
+                return end();
+
+            *position = utility::move(value);
+            return position;
         }
 
         iterator insert(iterator position, T &&value) override {
-            if (position >= end() || position < begin())
+            if (!_check_iterator(position))
                 return end();
 
             const auto distance = position - begin();
@@ -130,11 +179,11 @@ namespace container {
          * @return An iterator to the inserted elements position or end() in case of failure.
          */
         iterator insert(iterator position, iterator _begin, iterator _end) override {
-            if (position >= end() || position < begin() || _begin > _end || _end - _begin > end() - position)
+            if (!_check_iterator(position) || _begin > _end || _end - _begin > end() - position)
                 return end();
 
             // different logic if inserting into itself
-            if (_begin >= begin() && _begin < end())
+            if (_check_iterator(_begin))
                 return _insert_overlap(position, _begin, _end);
 
             auto remaining_till_end = end() - position; // remaining elements till end from position
@@ -176,6 +225,17 @@ namespace container {
             }
 
             return position;
+        }
+
+        /**
+         * Check if the provided iterator is in range [begin(), end()[
+         *
+         * @param iter
+         *
+         * @return true if the iterator is in [begin(), end()[, false otherwise
+         */
+        bool _check_iterator(iterator iter) {
+            return iter >= begin() && iter < end();
         }
     };
 }
