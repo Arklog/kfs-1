50e2039b5b3858f734e094abc0494107e66f91cb
Pierre <pierr.ducloux@gmail.com>
2026-02-03 17:00:30 +0100

    documented math

---
+++ examples/lib/math/math.cpp
@@ -0,0 +1,19 @@
+//
+// Created by pierre on 2/3/26.
+//
+
+/**
+* @example math.cpp
+*/
+
+#include "lib/math/math.hpp"
+
+//![math min]
+math::min(1, 0); //0
+math::min(1, 1); //1
+//![math min]
+
+//![math max]
+math::max(1, 0); //1
+math::max(1, 1); //1
+//![math max]
