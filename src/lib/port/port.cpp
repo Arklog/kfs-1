3dbff2a67da97bd0c8b44d35e2efad13f104dcb2
Pierre <pierr.ducloux@gmail.com>
2026-08-13 14:21:47 +0200

    added test and non test targets to all libraries

--- src/lib/port/port.cpp
+++ src/lib/port/port.cpp
@@ -5,11 +5,19 @@
 #include "port.hpp"
 
 uint8_t inb(uint16_t port) {
+#ifdef KFS_HOST_TEST
+    return 0;
+#else
     uint8_t ret;
     asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
+#endif
 }
 
 void outb(uint16_t port, uint8_t val) {
+#ifdef KFS_HOST_TEST
+    return ;
+#else
     asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+#endif
 }
