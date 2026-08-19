c7cc3f22eb1fa0ee6591e31a7255c2e359e9788c
Pierre <pierr.ducloux@gmail.com>
2026-08-19 20:26:04 +0200

    update

--- docker/mermaid/entrypoint.sh
+++ docker/mermaid/entrypoint.sh
@@ -6,40 +6,19 @@ cd /build/documentation || {
 }
 
 CURRENT_DIR=$(pwd)
-MERMAID_DIR="mermaid"
+MERMAID_DIR="source"
 
 OLF_IFS=${IFS}
 ICON_PACKS=(@iconify-json/{material-symbols,mdi,solar})
 IFS=" "
 MMDC="/home/mermaidcli/node_modules/.bin/mmdc -p /puppeteer-config.json --iconPacks ${ICON_PACKS[*]}"
 IFS=${OLF_IFS}
-for DIR_TO_PROCESS in $(find ${MERMAID_DIR} -type d); do
-  if [[ "${DIR_TO_PROCESS}" == "${MERMAID_DIR}" ]]; then
-    continue
-  fi
 
-  echo "Processing ${DIR_TO_PROCESS}"
+for FILE in $(find ${MERMAID_DIR} -type f -name '*.mmd'); do
+  echo "Processing ${FILE}"
 
-
-  cd "${DIR_TO_PROCESS}" || {
-    echo "Could not cd into ${DIR_TO_PROCESS}, exiting"
-     exit 1
-  }
-
-  FILES=$(find . -maxdepth 1 -type f -name '*.mmd')
-
-  for FILE in ${FILES}; do
-    SVG_FILE="${FILE%%.mmd}.svg"
-    ${MMDC} -i "${FILE}" -o "${SVG_FILE}"
-  done
-
-  cd "${CURRENT_DIR}" || {
-    echo "Could not restore current directory, exiting"
-    exit 1
-  }
+  SVG_FILE="${FILE%%.mmd}.svg"
+  ${MMDC} -i "${FILE}" -o "${SVG_FILE}" &
 done
 
-for SVG_FILE in $(find ${MERMAID_DIR} -type f -name '*.svg'); do
-  OUT_FILE="source/${SVG_FILE##"${MERMAID_DIR}"}"
-  mv "${SVG_FILE}" "${OUT_FILE}"
-done
+wait
